d2200e2f58a1f8e2dc529abe282f6a8926812e65
abrotor <abrotor@uoc.edu>
2025-11-26 11:36:04 +0100

    x

--- 2d/ej2d2.py
+++ 2d/ej2d2.py
@@ -68,12 +68,26 @@ Exemple:
 
 def calculate_max_and_min(list_numbers):
     # Write here your code
+    numMax = -10000
+    numMin = 10000
+    if len(list_numbers) < 1:
+        return ValueError("ValueError") 
+    for num in list_numbers:
+        if type(num) == str:
+            return TypeError("TypeError")
+        else:
+            if num > numMax:
+                numMax = num
+            if num < numMin:
+                numMin = num
+    return numMin, numMax
+
+        
+
     pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# print(
-#     "\nResult: ", calculate_max_and_min([10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55])
-# )
+print("\nResult: ", calculate_max_and_min([10, 5.1, 0, -2, 31, 55, 70, -10, 200, -55.55]))
--- 2d/ej2d3.py
+++ 2d/ej2d3.py
@@ -53,16 +53,20 @@ Exemples:
 
 def division_list(list_numbers: List, number: int) -> List[float]:
     result = []
+    if type(number)!= int and type(number)!= float:
+        raise IndexError(f"Value {number} is not numeric.")
     for number_in_list in list_numbers:
-        if not isinstance(number_in_list, str):
+        if type(number_in_list)!= int and type(number_in_list)!= float:
             raise IndexError(f"Value {number_in_list} is not numeric.")
+        else: 
+            number_in_list = number_in_list/number
         result.append(number_in_list)
     return result
 
 
-list_numbers = [1.5, 2.5, "9.2", 0, 22]
-scalar_number = "4.0"
+list_numbers = [1.5, 2.5, 9.2, 0, 22]
+scalar_number = 4.0
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(division_list(list_numbers, scalar_number))
+print(division_list(list_numbers, scalar_number))
